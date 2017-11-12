a40031041d9b1ac42305d2e007da2736d9ced814
chwe17 <21057365+chwe17@users.noreply.github.com>
2017-11-12 08:03:24 +0100

    cleanup

--- pyA20/gpio/mapping/orangepizero.h
+++ pyA20/gpio/mapping/orangepizero.h
@@ -33,56 +33,76 @@
 Structure that describe all gpio
 */
 typedef struct _pin {
-    char name[10];          // The processor pin
-    int offset;             // Memory offset for the pin
-    int pin_number;         // Number on the connector
+	char name[10];          // The processor pin
+	int offset;             // Memory offset for the pin
+	int pin_number;         // Number on the connector
 }pin_t;
 
 typedef struct _gpio {
-    char connector_name[10];
-    pin_t pins[41];
+	char connector_name[10];
+	pin_t pins[41];
 }gpio_t;
 
 
 gpio_t gpio[] = {
 
-    /*
-    #define PIN_PG0		SUNXI_GPG(0)
-*/  {"gpio1",
-        {
-            {   "PA0",  SUNXI_GPA(0),  13   },
-            {   "PA1",  SUNXI_GPA(1),  11   },
-            {   "PA2",  SUNXI_GPA(2),  22   },
-            {   "PA3",  SUNXI_GPA(3),  15   },
-            {   "PA6",  SUNXI_GPA(6),  7   },
-            {   "PA7",  SUNXI_GPA(7),  12   },
-            {   "PA10",  SUNXI_GPA(10),  26  },
-            {   "PA11",  SUNXI_GPA(11),  5  },
-            {   "PA12",  SUNXI_GPA(12),  3  },
-            {   "PA13",  SUNXI_GPA(13),  24  },	
-            {   "PA14",  SUNXI_GPA(14),  23  },
-            {   "PA15",  SUNXI_GPA(15),  19  },
-            {   "PA16",  SUNXI_GPA(16),  21  },
-            {   "PA18",  SUNXI_GPA(18),  18  },
-            {   "PA19",  SUNXI_GPA(19),  16  },
-            {   "PG6",  SUNXI_GPG(6),  8   },
-            {   "PG7",  SUNXI_GPG(7),  10   },
-            {
-                {   0,  0,  0}
-            },
-        }
-    },
+/*
+Description of Pinheader, please don't change pinnumer to keep this library consistent! A 'RPi compatible'
+pin header (26pin 'model A') looks like the following:
+
+								3.3V	|··|	5V
+(PA12, TWI0_SDA)				GPIO2	|··|	5V
+(PA11, TWI0_SCK)				GPIO3	|··|	GND
+(PA6, PMW1)						GPIO4	|··|	GPIO14	(PG6, UART1_TX)
+								GND		|··|	GPIO15	(PG7, UART1_RX)
+(PA1, UART2_RX)					GPIO17	|··|	GPIO18	(PA7)
+(PA0, UART2_TX)					GPIO27	|··|	GND
+(PA2, UART2_CTS)				GPIO22	|··|	GPIO23	(PA19, TWI1_SDA)
+								3.3V	|··|	GPIO24	(PA18, TWI1 SCK)
+(PA15, SPI1_MOSI, UART3_RTS)	GPIO10	|··|	GND
+(PA16, SPI1_MISO, UART3_CTS)	GPIO9	|··|	GPIO25	(PA2, UART2_RTS)
+(PA14, SPI1_CLK)				GPIO11	|··|	GPIO8	(PA13, SPI1_CS, UART3_TX)
+								GND		|··|	GPIO7	(PA10)
+
+*/  
+	{"GPIO",
+		{
+			//'first row'
+			{   "PA12",  SUNXI_GPA(12),  2   },
+			{   "PA11",  SUNXI_GPA(11),  3   },
+			{   "PA6",  SUNXI_GPA(),6  4   },
+			{   "PA1",  SUNXI_GPA(1),  17   },
+			{   "PA0",  SUNXI_GPA(0),  27   },
+			{   "PA2",  SUNXI_GPA(2),  22   },
+			{   "PA15",  SUNXI_GPA(15),  10  },
+			{   "PA16",  SUNXI_GPA(16),  9  },
+			{   "PA14",  SUNXI_GPA(14),  11  },
+			//'second row'
+			{   "PG6",  SUNXI_GPG(6),  14  },	
+			{   "PG7",  SUNXI_GPG(7),  15  },
+			{   "PA7",  SUNXI_GPA(7),  18  },
+			{   "PA19",  SUNXI_GPA(19),  23  },
+			{   "PA18",  SUNXI_GPA(18),  24  },
+			{   "PA2",  SUNXI_GPA(2),  25  },
+			{   "PA13",  SUNXI_GPA(13),  8   },
+			{   "PA10",  SUNXI_GPA(10),  7   },
+			{
+				{   0,  0,  0}
+			},
+		}
+		},
   /*
     #define PIN_PG0		SUNXI_GPG(0)
-*/  {"LED",
-        {
-            {   "POWER_LED",  SUNXI_GPL(10),  1   },
-            {   "STATUS_LED",  SUNXI_GPA(17),  2   },
-            {
-                {   0,  0,  0}
-            },
-        }
-    },
+*/
+	{"LED",
+		{
+			{   "POWER_LED",  SUNXI_GPL(10),  1   },
+			{   "STATUS_LED",  SUNXI_GPA(17),  2   },
+			{
+				{   0,  0,  0}
+			},
+		}
+	},
 };
 
 
